5d8614cb8e7e1edba413c7a8e5edcf81650d0807
hughes03soft <hughes03.soft@gmail.com>
2022-04-16 18:40:02 +0800

    added test project + test for startup message

---
+++ HorseTrackAtmTests/HorseTrackAtmTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HorseTrackAtm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorseTrackAtm.Tests
+{
+    [TestClass()]
+    public class HorseTrackAtmTests
+    {
+        private HorseTrackAtm _atm = new HorseTrackAtm();
+
+        [TestMethod()]
+        public void GetStartupMessageTest()
+        {
+            _atm.Load(new DefaultHorseTrackAtmDataLoader());
+            var actual = _atm.GetStartupMessage();
+
+            string expected =
+                "Inventory:" + Environment.NewLine +
+                "$1, 10" + Environment.NewLine +
+                "$5, 10" + Environment.NewLine +
+                "$10, 10" + Environment.NewLine +
+                "$20, 10" + Environment.NewLine +
+                "$100, 10" + Environment.NewLine +
+                "Horses:" + Environment.NewLine +
+                "1, That Darn Gray Cat, 5" + Environment.NewLine +
+                "2, Fort Utopia, 10" + Environment.NewLine +
+                "3, CountSheep, 9" + Environment.NewLine +
+                "4, Ms Traitour, 4" + Environment.NewLine +
+                "5, Real Princess, 3" + Environment.NewLine +
+                "6, Pa Kettle, 5" + Environment.NewLine +
+                "7, Gin Stinger, 6" + Environment.NewLine;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
