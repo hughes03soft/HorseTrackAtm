e8d76e6ec5ee66850ed8490beb4b00cf19c65ecb
hughes03soft <hughes03.soft@gmail.com>
2022-04-16 17:34:56 +0800

    added project template and stubs

---
+++ HorseTrackAtm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorseTrackAtm
+{
+    class HorseTrackAtm
+    {
+        public bool IsQuitting { get; }
+
+        public void ShowStartupMessage()
+        {
+            Console.WriteLine("Starting up...");
+        }
+
+        public void ProcessInput(string input)
+        {
+            Console.WriteLine("Processing input: '{0}'", input);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorseTrackAtm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var atm = new HorseTrackAtm();
+            atm.ShowStartupMessage();
+
+            bool quit = false;
+
+            while(quit == false)
+            {
+                var input = Console.ReadLine();
+                atm.ProcessInput(input);
+                quit = atm.IsQuitting;
+            }
+        }
+    }
+}
